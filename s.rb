6cf086b207c00c3cffd9653418b0020dab3c2f74
Adam La Rosa <adam.larosa@gmail.com>
2020-03-07 18:03:13 -0800

    recursive people method

---
+++ s.rb
@@ -0,0 +1,25 @@
+require 'rest-client'
+require 'pry'
+require 'json'
+
+class Swapi
+    def initialize
+	@array = [] 
+    end
+
+    def get_people(path)
+	people = JSON.parse(RestClient.get(path))
+	people["results"].each { |person| @array << person }
+	return "complete" if people["next"] == nil
+	get_people(people["next"])
+    end
+
+    def people
+	@array
+    end
+end
+
+p = Swapi.new
+p.get_people("http://www.swapi.co/api/people/")
+binding.pry
+p
