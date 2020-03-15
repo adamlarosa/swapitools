495f61b91e24afa33c54cfeefafe00f0884ab30b
Adam La Rosa <adam.larosa@gmail.com>
2020-03-14 21:32:01 -0700

    planets fetch method

--- s.rb
+++ s.rb
@@ -15,7 +15,17 @@ require 'json'
 
 class Swapi
     def initialize
-	@people = [] 
+	@people = []
+	@planets = []
+    end
+
+    def get_planets(path)
+	print "."
+	planets = JSON.parse(RestClient.get(path))
+	planets["results"].each { |planet| @planets << planet }
+	return "complete" if planets["next"] == nil
+	get_planets(planets["next"])
+
     end
 
     def get_people(path)
@@ -27,16 +37,19 @@ class Swapi
     end
 
     def find_person(person)
-	@people.find {|p| p["name"].include?(person)}
-	
+	@people.find {|p| p["name"].include?(person)}	
     end
 
     def people
 	@people
     end
+
 end
 
 p = Swapi.new
+print "Getting People"
 p.get_people("http://www.swapi.co/api/people/")
+print "\nGetting Planets"
+p.get_planets("http://www.swapi.co/api/planets")
 binding.pry
 p
